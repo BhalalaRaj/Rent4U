8a2f67926cdce3ceec50f05550a742694383d247
Bhushan <bhushanmaniyar@gmail.com>
2020-10-30 10:46:34 +0530

    Dashboard design for user

---
+++ app/src/main/java/com/example/dell/rent4u/Splash_Screen.java
@@ -0,0 +1,39 @@
+package com.example.dell.rent4u;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    final int SCREEN_TIMER = 3000;
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        getSupportActionBar().hide();
+
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash_Screen.this, Dashboard.class));
+                finish();
+            }
+        },SCREEN_TIMER);
+
+    }
+}
