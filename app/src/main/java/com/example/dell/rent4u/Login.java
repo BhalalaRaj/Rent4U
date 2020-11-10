042ee3bc4e1411850e101cfc9aa16026e90bdeb0
Bhushan <bhushanmaniyar@gmail.com>
2020-11-10 17:49:44 +0530

    User Registration and Login complete

--- app/src/main/java/com/example/dell/rent4u/Login.java
+++ app/src/main/java/com/example/dell/rent4u/Login.java
@@ -1,17 +1,29 @@
 package com.example.dell.rent4u;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import soup.neumorphism.NeumorphButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
     TextView user, provider;
     NeumorphButton login;
+    EditText email, password;
+
+    // Firebase
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +33,20 @@ public class Login extends AppCompatActivity {
         user = findViewById(R.id.user);
         provider = findViewById(R.id.provider);
         login = findViewById(R.id.login);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Login.this, Dashboard.class));
+            public void onClick(View v){
+                if(!(email.getText().toString().trim().equals("") && password.getText().toString().trim().equals(""))){
+                    startLogin(email.getText().toString().trim(), password.getText().toString().trim());
+                } else {
+                    Toast.makeText(Login.this,"Empty Fields",Toast.LENGTH_SHORT).show();
+                }
             }
+
         });
 
         user.setOnClickListener(new View.OnClickListener() {
@@ -43,5 +63,17 @@ public class Login extends AppCompatActivity {
             }
         });
 
+    }
+    public void startLogin(String email, String password){
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(Login.this, Dashboard.class));
+                } else {
+                    Toast.makeText(Login.this, "Invalid credentials :(", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dell/rent4u/UserRegistration.java
+++ app/src/main/java/com/example/dell/rent4u/UserRegistration.java
@@ -1,14 +1,100 @@
 package com.example.dell.rent4u;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import soup.neumorphism.NeumorphButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserRegistration extends AppCompatActivity {
+
+    EditText username, usermobile, useremail, userpassword, useraddress, usercity, usercity_pincode;
+    NeumorphButton submit;
+    FirebaseAuth mAuth;
+    FirebaseUser userId;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        username = findViewById(R.id.username);
+        usermobile = findViewById(R.id.usermobile);
+        useremail = findViewById(R.id.useremail);
+        userpassword = findViewById(R.id.userpassword);
+        useraddress = findViewById(R.id.useraddress);
+        usercity = findViewById(R.id.usercity);
+        usercity_pincode = findViewById(R.id.usercity_pincode);
+
+        submit = findViewById(R.id.submit);
+        mAuth = FirebaseAuth.getInstance();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crateAcc(useremail.getText().toString().trim(), userpassword.getText().toString().trim());
+            }
+        });
+    }
+
+    private void crateAcc(String useremail, String userpassword) {
+
+        mAuth.createUserWithEmailAndPassword(useremail, userpassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in userId's information
+                            userId = mAuth.getCurrentUser();
+                            Map<String, String> data = new HashMap<>();
+                            data.put("Name", username.getText().toString().trim());
+                            data.put("Mobile_no", usermobile.getText().toString().trim());
+                            data.put("Email", useremail);
+                            data.put("Password", userpassword);
+                            data.put("Address", useraddress.getText().toString().trim());
+                            data.put("City", usercity.getText().toString().trim());
+                            data.put("City_PinCode", usercity_pincode.getText().toString().trim());
+                            data.put("License", "false");
+
+                            writeUser(data);
+
+                            Toast.makeText(UserRegistration.this, "Authentication Successful.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the userId.
+                            Toast.makeText(UserRegistration.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void writeUser(Map<String, String> data) {
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Customer").child(userId.getUid());
+        mDatabase.setValue(data);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userId.getUid());
+        Map<String, String> a = new HashMap<>();
+        a.put(userId.getUid(),"Customer");
+        mDatabase.setValue(a);
+        startActivity(new Intent(UserRegistration.this, Login.class));
+        Toast.makeText(UserRegistration.this, "Your data Successfully Registered now you can Login", Toast.LENGTH_LONG).show();
+        finish();
     }
 }
