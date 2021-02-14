fb2f242046cb18e98424a8060542198bb5ee80db
Raj Bhalala <rajbhalala.rb@gmail.com>
2021-02-14 15:23:50 +0530

    view vehicle

---
+++ app/src/main/java/com/example/dell/rent4u/ViewVehicle.java
@@ -0,0 +1,14 @@
+package com.example.dell.rent4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewVehicle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_vehicle);
+    }
+}
