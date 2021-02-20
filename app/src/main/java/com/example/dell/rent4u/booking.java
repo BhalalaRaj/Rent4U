6373dc98faa171041ffbe10575b282c91b0204a4
Raj Bhalala <rajbhalala.rb@gmail.com>
2021-02-20 15:03:23 +0530

    datepicker & booking page

--- app/src/main/java/com/example/dell/rent4u/booking.java
+++ app/src/main/java/com/example/dell/rent4u/booking.java
@@ -2,15 +2,21 @@ package com.example.dell.rent4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class booking extends AppCompatActivity {
 
     TextView name,tvRent,seating;
-    EditText DateFrom,DateTo;
+    EditText DateFrom,DateTo,Source,Destination;
+    DatePickerDialog datepicker;
     Button BookNow;
 
     @Override
@@ -23,7 +29,48 @@ public class booking extends AppCompatActivity {
 
         DateFrom = findViewById(R.id.DateFrom);
         DateTo = findViewById(R.id.DateTo);
+        Source = findViewById(R.id.Source);
+        Destination = findViewById(R.id.Destination);
         BookNow = findViewById(R.id.BookNow);
+
+        DateFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                datepicker = new DatePickerDialog(booking.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                DateFrom.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                datepicker.show();
+            }
+        });
+
+        DateTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                datepicker = new DatePickerDialog(booking.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                DateTo.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                datepicker.show();
+            }
+        });
+
 
     }
 }
